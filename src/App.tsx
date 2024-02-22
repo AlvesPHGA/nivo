5f017586648748ee6887cf5d28e29773061c26a9
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-22 11:21:27 -0300

    updated: was added some components in app tsx, as Input and Button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { Plus } from 'lucide-react';
+import { FileDown, Plus, Search } from 'lucide-react';
 import { Header } from './components/global/Header/Header';
 import { Tabs } from './components/Tabs/Tabs';
+import { Button } from './components/ui/Button';
+import { Control, Input } from './components/ui/Input';
 
 export function App() {
    return (
@@ -12,10 +14,22 @@ export function App() {
          <main className="max-w-6xl mx-auto space-y-5">
             <div className="flex items-center gap-3">
                <h1 className="text-xl font-bold">Tags</h1>
-               <button className="inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1">
+               <Button variant="primary">
                   <Plus className="size-3" />
                   Create new
-               </button>
+               </Button>
+            </div>
+
+            <div className="flex items-center justify-between">
+               <Input variant="filter">
+                  <Search className="size-3" />
+                  <Control placeholder="Search tags..." />
+               </Input>
+
+               <Button>
+                  <FileDown className="siz-3" />
+                  Export
+               </Button>
             </div>
          </main>
       </div>
