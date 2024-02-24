908c8bb95d9a463ff26a720da9b9d6e72cb16625
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-02-24 17:49:24 -0300

    update: tabs component, was added content in component

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,7 +1,41 @@
+import { Code2, ListVideo, Settings, Tags } from 'lucide-react';
+
 export function Tabs() {
    return (
-      <section>
-         <p>tabs</p>
+      <section className="border-b border-zinc-800 py-4">
+         <nav>
+            <a
+               href=""
+               className="py-1.5 px-3 bg-zinc-800 text-zinc-100 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent"
+            >
+               <ListVideo className="size-4" />
+               Uploads
+            </a>
+
+            <a
+               href=""
+               className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800"
+            >
+               <Tags className="size-4" />
+               Tags
+            </a>
+
+            <a
+               href=""
+               className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800"
+            >
+               <Settings className="size-4" />
+               Settings
+            </a>
+
+            <a
+               href=""
+               className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800"
+            >
+               <Code2 className="size-4" />
+               Developers
+            </a>
+         </nav>
       </section>
    );
 }
